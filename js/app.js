4f45b3f34d3582273af1bc0be1ac625354a1df40
jimenez2society <121840097+jimenez2society@users.noreply.github.com>
2023-03-21 00:47:55 -0600

    add styles for the generator container made it a grid to seperate options and the box where the password will display

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@
 import Toast from "./Toast.js";
 
 let onPasswordGenerate = new Toast();
+function showGeneratedPassword() {
+  let password = generatePassword();
+  document.querySelector(".generated-password--text").innerHTML = password;
+}
+function generatePassword() {
+  return "TESTING";
+}
+function gatherPasswordRestriction() {}
 
 document.querySelector(".logo").addEventListener("click", (e) => {
   onPasswordGenerate.activateSuccess("Generated password successfully");
